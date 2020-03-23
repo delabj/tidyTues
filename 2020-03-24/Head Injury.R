6d3b7d61b8f67b430acef894407a2a75e72f2cb0
Joshua de la Bruere <j.l.delabruere@gmail.com>
2020-03-23 14:26:15 -0500

    Create Head Injury.R

---
+++ 2020-03-24/Head Injury.R
@@ -0,0 +1,3 @@
+tbi_age <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-24/tbi_age.csv')
+tbi_year <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-24/tbi_year.csv')
+tbi_military <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2020/2020-03-24/tbi_military.csv')
